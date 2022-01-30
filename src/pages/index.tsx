d1173c4d112eb6e4219318acfaa14c468aa7e0f9
leo <celsolnv27@gmail.com>
2022-01-30 20:57:47 -0300

    feature: adding mode preview enabled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Prismic from '@prismicio/client';
 import Link from 'next/link';
 import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
-import styles from './home.module.scss';
 import { PostPreview } from '../components/PostPreview';
 import Header from '../components/Header';
+import styles from './home.module.scss';
 
 interface Post {
   uid?: string;
@@ -25,17 +25,22 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-export default function Home({ postsPagination }: HomeProps): JSX.Element {
+export default function Home({
+  postsPagination,
+  preview,
+}: HomeProps): JSX.Element {
   const { next_page, results } = postsPagination;
   const [posts, setPosts] = useState(results);
   const [nextPage, setNextPage] = useState(next_page);
+
   async function handleLoadMorePosts(): Promise<void> {
     const req = await (await fetch(next_page)).json();
     const newPosts = posts;
     req.results.forEach(post => {
-      const formatedPost = {
+      const formattedPost = {
         uid: post.uid,
         first_publication_date: post.first_publication_date,
         data: {
@@ -44,11 +49,12 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
           author: post.data.author,
         },
       };
-      newPosts.push(formatedPost);
+      newPosts.push(formattedPost);
     });
     setPosts(newPosts);
     setNextPage(req.next_page);
   }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -70,16 +76,30 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
           </button>
         )}
       </main>
+      <footer>
+        {preview && (
+          <aside>
+            <Link href="/api/exit-preview">
+              <a> Sair do modo Preview</a>
+            </Link>
+          </aside>
+        )}
+      </footer>
     </div>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  console.log(preview, previewData);
   const prismic = getPrismicClient();
   const response = await prismic.query(
     Prismic.Predicates.at('document.type', 'posts'),
     {
       pageSize: 1,
+      ref: previewData?.ref ?? null,
     }
   );
   const next_page = response.next_page ?? '';
@@ -101,6 +121,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       postsPagination,
+      preview,
     },
   };
 };
